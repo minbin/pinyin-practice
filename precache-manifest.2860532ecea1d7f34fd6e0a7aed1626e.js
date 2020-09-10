a419d82d793abfeb12d966b1d9ec3a3684e664dd
tammy <tammylam0@gmail.com>
2020-09-10 17:16:26 -0500

    Updates

---
+++ precache-manifest.2860532ecea1d7f34fd6e0a7aed1626e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86067137680554b1e198a0bfc1a1c839",
+    "url": "/pinyin-practice/index.html"
+  },
+  {
+    "revision": "a4d0ef8fb43e805ed861",
+    "url": "/pinyin-practice/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "678158a6d71481f5a8f7",
+    "url": "/pinyin-practice/static/css/main.6fe92a10.chunk.css"
+  },
+  {
+    "revision": "a4d0ef8fb43e805ed861",
+    "url": "/pinyin-practice/static/js/2.3231f1a5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pinyin-practice/static/js/2.3231f1a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678158a6d71481f5a8f7",
+    "url": "/pinyin-practice/static/js/main.d5c9f4cf.chunk.js"
+  },
+  {
+    "revision": "83463bccfb378803405d",
+    "url": "/pinyin-practice/static/js/runtime-main.a382d287.js"
+  }
+]);
